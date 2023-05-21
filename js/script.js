ba31d92380674a124ef1cf8ef02dcd54e9994816
om <90174038+omchaudhari1107@users.noreply.github.com>
2023-05-21 06:59:32 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var options = {
+  strings: ["Full-Stack Dev","Intern","Programmer","Learner"],
+  typeSpeed:100 ,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+};
+
+var typed = new Typed('.multiple-filed', options);
